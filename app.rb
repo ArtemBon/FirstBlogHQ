1c1f3d82e8c3d95bff009091fd097b2ca82d35ac
Artem Bondarets <tyom.bond@gmail.com>
2019-06-30 13:16:42 +0300

    Add Post migration

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :database, "sqlite3:firstblog.db"
+
+class Post < ActiveRecord::Base
+end
+
 get '/' do
 	erb "First Blog"			
 end
